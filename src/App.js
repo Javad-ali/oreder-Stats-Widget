1a73d906fe9634a51671754771251d8248d1509b
JavadAli <javadalivpk45@gmail.com>
2023-03-03 16:30:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Widget_Order from './pages/widget/Widget_Order';
+
+function App() {
+  return (
+    <div className="App">
+      <Widget_Order/>
+    </div>
+  );
+}
+
+export default App;
